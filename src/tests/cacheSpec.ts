9685333e34f1bcc6154496a51a64d71896d16a3f
Abdelaziz Moustafa Elgeab <abdelaziz.mustafa.my@gmail.com>
2022-12-23 00:54:46 +0300

    most files added

---
+++ src/tests/cacheSpec.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+import supertest from 'supertest';
+import app from '..';
+import { getImage, getImagesDir } from '../utils/utils';
+
+const request = supertest(app);
+const imgsDir = getImagesDir();
+const cachePath = path.join(imgsDir,'cache');
+
+describe ("Image processing and caching", (): void => {
+    beforeEach( () => {
+        console.log('before each');
+    });
+    beforeAll( () => {
+        console.log('before all');
+        if (fs.existsSync(cachePath)) {
+            fs.rmdirSync(cachePath,{recursive:true})
+        }
+    });
+    afterEach( () => {
+        console.log('after each');
+    });
+    afterAll( () => {
+        console.log('after all');
+    });
+    it('tests getImagesDir', async ():Promise<void> => {
+        const response = await fs.existsSync(imgsDir);
+        expect(response).toBe(true);
+    });
+    it('tests getImagesDir', async ():Promise<void> => {
+        const fileTest = await request.get('/api/images?filename=fjord&height=400&width=600');
+        const response = fs.existsSync(cachePath);
+        expect(response).toBe(true);
+    });    
+})
+
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -8,7 +8,7 @@ const request = supertest(app);
 const imgsDir = getImagesDir();
 const cachePath = path.join(imgsDir,'cache');
 
-describe ("Test endpoint responses", () => {
+describe ("Test endpoint responses", (): void => {
     beforeEach( () => {
         console.log('before each');
     });
@@ -22,15 +22,29 @@ describe ("Test endpoint responses", () => {
         console.log('after each');
     });
     afterAll( () => {
-        console.log('after each');
+        console.log('after all');
     });
-    it('tests endpoint with no filename', async () => {
-        const response = await request.get('/image');
-        expect(response.status).toBe(400);
+    it('tests endpoint with /', async ():Promise<void> => {
+        const response = await request.get('/');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('{"message":"To start, please add /api/images?filename=filenae&<optional>height=10&<optional>width=20 to the address bar"}');
     });
-    it('tests endpoint with invalid filename', async () => {
-        const response = await request.get('/image').query({filename: 'notthere'});
+    it('tests endpoint with no filename', async ():Promise<void> => {
+        const response = await request.get('/api/image');
         expect(response.status).toBe(404);
+    });
+    it('tests endpoint with invalid filename', async ():Promise<void> => {
+        const response = await request.get('/api/image').query({filename: 'notthere'});
+        expect(response.status).toBe(404);
+    });
+    it('tests endpoint with valid filename', async ():Promise<void> => {
+        const response = await request.get('/api/images?filename=fjord');
+        expect(response.status).toBe(200);
+    });
+    it('tests endpoint with valid arguments', async ():Promise<void> => {
+        const response = await request.get('/api/images?filename=fjord&height=400&width=600');
+        expect(response.status).toBe(200);
     });
 
 })
+
